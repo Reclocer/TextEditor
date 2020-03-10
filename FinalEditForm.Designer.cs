38f9f17c12b99de55f887bc426c1f509d115014d
Reclocer <54275125+Reclocer@users.noreply.github.com>
2020-03-10 23:55:01 +0300

    Add FinalEditForm + Save function

---
+++ FinalEditForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Text_Editor_0._1
+{
+    partial class FinalEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FinalEditForm));
+            this.FinalTextBox = new System.Windows.Forms.TextBox();
+            this.FinalTextSaveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FinalTextBox
+            // 
+            this.FinalTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FinalTextBox.Location = new System.Drawing.Point(12, 12);
+            this.FinalTextBox.Multiline = true;
+            this.FinalTextBox.Name = "FinalTextBox";
+            this.FinalTextBox.Size = new System.Drawing.Size(1386, 689);
+            this.FinalTextBox.TabIndex = 0;
+            // 
+            // FinalTextSaveBtn
+            // 
+            this.FinalTextSaveBtn.Location = new System.Drawing.Point(12, 707);
+            this.FinalTextSaveBtn.Name = "FinalTextSaveBtn";
+            this.FinalTextSaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.FinalTextSaveBtn.TabIndex = 1;
+            this.FinalTextSaveBtn.Text = "Сохранить";
+            this.FinalTextSaveBtn.UseVisualStyleBackColor = true;
+            this.FinalTextSaveBtn.Click += new System.EventHandler(this.FinalTextSaveBtn_Click);
+            // 
+            // FinalEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 743);
+            this.Controls.Add(this.FinalTextSaveBtn);
+            this.Controls.Add(this.FinalTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FinalEditForm";
+            this.Text = "Финальное редактирование";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FinalTextBox;
+        private System.Windows.Forms.Button FinalTextSaveBtn;
+    }
+}
---
+++ FinalEditForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Editor_0._1
+{
+    public partial class FinalEditForm : Form
+    {
+        private Form1 _form1 = new Form1();
+        private SaveFileDialog _saveFileDialog = new SaveFileDialog();
+
+        public FinalEditForm(Form1 form)
+        {
+            InitializeComponent();
+            _form1 = form;            
+            Start();
+        }       
+
+        private void Start()
+        {
+            if (_form1.GetOutputText() != "Output text")
+            {
+                FinalTextBox.Text = _form1.GetOutputText();
+            }
+            else
+            {
+                FinalTextBox.Text = _form1.GetInputText();
+            }
+        }
+
+        private async void FinalTextSaveBtn_Click(object sender, EventArgs e)
+        {
+            _saveFileDialog.Filter = "Text files(*.txt)|*.txt|Docx(*.docx)|*.docx";
+
+            if (_saveFileDialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            string fileName = _saveFileDialog.FileName;
+
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Append))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                {
+                    await streamWriter.WriteLineAsync(FinalTextBox.Text);
+                }
+            }          
+
+        }
+
+    }
+}
